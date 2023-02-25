c53b3347910faf3edc32edd191e8aa76f01097f2
Jon Keam <jpkeam@gmail.com>
2023-02-25 15:30:37 -0500

    add sleep to wait for operator

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ oc new-project gitlab
 oc apply -f https://github.com/jetstack/cert-manager/releases/download/v1.7.1/cert-manager.yaml
 oc apply -f ./gitlab-runner-subscription.yaml
 oc apply -f ./gitlab-runner-secret.yaml
+echo 'waiting 3min for operator to install...'
+sleep 180
 oc apply -f ./gitlab-runner.yaml
